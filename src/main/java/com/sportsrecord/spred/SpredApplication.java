687fd3dd50b54415ef18c7f6a0f252404aee7735
Yeon <yennie37@naver.com>
2026-01-12 21:54:41 +0900

    First commit

---
+++ src/main/java/com/sportsrecord/spred/SpredApplication.java
@@ -0,0 +1,13 @@
+package com.sportsrecord.spred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportsrecord/spred/SpredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsrecord.spred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
