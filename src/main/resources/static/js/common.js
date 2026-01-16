9173536d368ad59846d3cdf407ceed29c96837c8
Yerim <yalmy.dev@gmail.com>
2026-01-16 18:33:40 +0900

    0116_css 분리, 아이콘 이미지 추가, 이니셜/로그인화면 퍼블작업

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,15 +1,22 @@
 function activeSpred(){
     setBodyHeight();
+    layoutResize();
+    /****************테스트용 함수(반영X)****************/
 }
-// ==================vh 단위 대응==================
-function setBodyHeight(){
+// ==================css 변수 선언==================
+function setBodyHeight(){ // vh 단위 대응
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', vh + 'px');
 }
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', vh + 'px');
-});
+function layoutResize(){ // 페이지 하단 여백
+    let footer = document.querySelector('.footer');
+
+    if(footer){
+        let height = footer.offsetHeight;
+
+        document.documentElement.style.setProperty('--layout-btm-height', height + 'px');
+    }
+}
 // ==================개별함수==================
 function modeChange(teamName){
     if(teamName === 'none'){
@@ -19,5 +26,19 @@ function modeChange(teamName){
         document.body.className = teamName;
     }
 }
+function trackScroll(){
 
+}
 window.addEventListener('load', activeSpred);
+window.addEventListener('resize', activeSpred);
+window.addEventListener('scroll', function(e){
+    let target = e.target;
+
+    if(target === document){
+        console.log("document is scrolling");
+    } else {
+        console.log('is scrolling', target);
+        console.log('scrolling is Id', target.id);
+        console.log('scrolling is class', target.class);
+    }
+}, true);
